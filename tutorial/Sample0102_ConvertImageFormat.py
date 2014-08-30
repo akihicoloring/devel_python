5db45bf6cb6ad9bf243390641afaa5a8cb6e0868
Akihiko Kaino <kaino@Misago.local>
2014-08-30 23:47:29 +0900

    add some templates for samples on tutorial.

---
+++ tutorial/Sample0102_ConvertImageFormat.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import scipy.misc
+import pylab
+import PIL.Image
+
+if __name__ == "__main__":
+    iFilename = '../data/lena.jpg'
+    oFilename = '/tmp/test.jpg'
+
+    
---
+++ tutorial/Sample0105_PCAOfImages.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# p.27
+
+import scipy.misc
+import pylab
+import PIL.Image
+
+if __name__ == "__main__":
+    iFilename = '../data/lena.jpg'
+    oFilename = '/tmp/test.jpg'
+
+    
